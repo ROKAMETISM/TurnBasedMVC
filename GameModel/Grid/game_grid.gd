30616a9ac018a0fc086e0763f6f715c65d2ed2e5
ROJS <mskim829@naver.com>
2025-11-27 23:14:09 +0900

    Update game_grid.gd

--- GameModel/Grid/game_grid.gd
+++ GameModel/Grid/game_grid.gd
@@ -1,24 +1,61 @@
-class_name GameGrid extends Node
+# Represents a grid with its size, the size of each cell in pixels, and some helper functions to
+# calculate and convert coordinates.
+# It's meant to be shared between game objects that need access to those values.
+class_name Grid
+extends Resource
 
-static var units : Dictionary[Vector3i, Unit]
+# The grid's size in rows and columns.
+@export var size := Vector2i(20, 20)
+# The size of a cell in pixels.
+@export var cell_size := Vector2i(80, 80)
 
-static func spawn_unit(unit : Unit, position : Vector3i)->bool:
-	var return_val := false
-	if units.keys().has(position) and units.get(position) and is_instance_valid(units.get(position)):
-		return_val = true
-	units.set(position, unit)
-	return return_val
+# Half of ``cell_size``.
+# We will use this to calculate the center of a grid cell in pixels, on the screen.
+# That's how we can place units in the center of a cell.
+var _half_cell_size : Vector2 = Vector2(cell_size) / 2.0
 
-static func get_unit(position : Vector3i)->Unit:
-	return units.get(position, null)
 
-static func remove_unit(unit : Unit)->bool:
-	if units.has(unit) and is_instance_valid(unit):
-		units.erase(unit)
-		return true
-	return false
+# Returns the position of a cell's center in pixels.
+# We'll place units and have them move through cells using this function.
+func calculate_map_position(grid_position: Vector2i) -> Vector2:
+	return Vector2(grid_position * cell_size) + _half_cell_size
 
-static func remove_unit_from_position(position : Vector3i)->bool:
-	if get_unit(position):
-		return remove_unit(get_unit(position))
-	return false
+
+# Returns the coordinates of the cell on the grid given a position on the map.
+# This is the complementary of `calculate_map_position()` above.
+# When designing a level, you'll place units visually in the editor. We'll use this function to find
+# the grid coordinates they're placed on, and call `calculate_map_position()` to snap them to the
+# cell's center.
+func calculate_grid_coordinates(map_position: Vector2) -> Vector2i:
+	return Vector2i((map_position / Vector2(cell_size)).floor())
+
+
+# Returns true if the `cell_coordinates` are within the grid.
+# This method and the following one allow us to ensure the cursor or units can never go past the
+# map's limit.
+func is_within_bounds(cell_coordinates: Vector2i) -> bool:
+	var out := cell_coordinates.x >= 0 and cell_coordinates.x < size.x
+	return out and cell_coordinates.y >= 0 and cell_coordinates.y < size.y
+
+
+# Makes the `grid_position` fit within the grid's bounds.
+# This is a clamp function designed specifically for our grid coordinates.
+# The Vector2 class comes with its `Vector2.clamp()` method, but it doesn't work the same way: it
+# limits the vector's length instead of clamping each of the vector's components individually.
+# That's why we need to code a new method.
+func clamp(grid_position: Vector2i) -> Vector2i:
+	var out := grid_position
+	out.x = clamp(out.x, 0, size.x - 1)
+	out.y = clamp(out.y, 0, size.y - 1)
+	return out
+
+
+# Given Vector2 coordinates, calculates and returns the corresponding integer index. You can use
+# this function to convert 2D coordinates to a 1D array's indices.
+#
+# There are two cases where you need to convert coordinates like so:
+# 1. We'll need it for the AStar algorithm, which requires a unique index for each point on the
+# graph it uses to find a path.
+# 2. You can use it for performance. More on that below.
+func as_index(cell: Vector2i) -> int:
+	return int(cell.x + size.x * cell.y)
