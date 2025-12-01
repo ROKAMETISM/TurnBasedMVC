c1c95211201667f8f7f9682522ee51b09c9d39b7
ROJS <mskim829@naver.com>
2025-12-01 22:37:19 +0900

    Init setup

--- GameModel/GameState/unit.gd
+++ GameModel/GameState/unit.gd
@@ -1,2 +1,11 @@
 class_name Unit
 var stat := UnitStats.new()
+var is_my_turn := false :
+	set(value):
+		if value == is_my_turn:
+			return
+		if value:
+			EventHandler.turn_started.emit(self)
+		else:
+			EventHandler.turn_ended.emit(self)
+		is_my_turn = value
---
+++ Main/event_handler.gd
@@ -0,0 +1,3 @@
+extends Node
+signal turn_started(unit : Unit)
+signal turn_ended(unit : Unit)
